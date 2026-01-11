e3744fd03234c36649f02dc10c5402683e639182
ianmartin16 <imartindm20@gmail.com>
2026-01-10 18:28:36 -0600

    Add some new features3

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,8 +1,14 @@
 // app/success/page.tsx
 import SuccessClient from "./SuccessClient";
 
-export default function SuccessPage({ searchParams }: any) {
-  const sessionId = searchParams?.session_id as string | undefined;
+export const dynamic = "force-dynamic"; // importante para que no lo intente pre-render estático
+
+export default function SuccessPage({
+  searchParams,
+}: {
+  searchParams: { session_id?: string };
+}) {
+  const sessionId = searchParams?.session_id || null;
+
   return <SuccessClient sessionId={sessionId} />;
 }
-
